1917a24d01178416feaeffdce7661d6cd43457e9
avidrutham <divyamathur89@gmail.com>
2019-09-05 23:07:23 +0530

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About me</h1>
+    <p>A personal about page</p>
+    <Link to="/">&larr; Back to home</Link>
+  </>
+);
